946480959c3882433f41eb3ca97e03d03653948e
TAksenov <taksenov@gmail.com>
2020-02-24 20:40:38 +0500

    BR-1: feat: add initial view of my homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,32 +8,31 @@ import styles from './styles.module.css';
 
 const features = [
   {
-    title: <>Easy to Use</>,
-    imageUrl: 'img/undraw_docusaurus_mountain.svg',
-    description: (
-      <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
-      </>
-    ),
-  },
-  {
-    title: <>Focus on What Matters</>,
-    imageUrl: 'img/undraw_docusaurus_tree.svg',
-    description: (
-      <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
-      </>
-    ),
-  },
-  {
-    title: <>Powered by React</>,
+    title: <>Наработки</>,
     imageUrl: 'img/undraw_docusaurus_react.svg',
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        <Link to={'docs/doc1'}>Здесь</Link> я делюсь своими мыслями и документацией к моим работам, 
+        которые систематизированы в моих{' '}
+        <Link to="https://github.com/taksenov?tab=repositories">репозиториях гитхаба</Link>.
+      </>   
+    ),
+  },
+  {
+    title: <>Блог</>,
+    imageUrl: 'img/undraw_docusaurus_tree.svg',
+    description: (
+      <>
+        <Link to={'blog'}>В блоге</Link> я пишу мысли и идеи, которые настолько ничтожны, что не заслуживают быть помещенными в серьезные разделы моей персональной страницы.
+      </>
+    ),
+  },
+  {
+    title: <>Книги</>,
+    imageUrl: 'img/undraw_docusaurus_mountain.svg',
+    description: (
+      <>
+        <Link to={'docs/mdx'}>В этом разделе</Link> я пишу конспекты по прочитанными мной книгам. Как правило я стараюсь уделять внимание фундаментальным и академическим трудам из области информатики.
       </>
     ),
   },
@@ -59,8 +58,8 @@ function Home() {
   const {siteConfig = {}} = context;
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`${siteConfig.title}`}
+      description="Персональная страница программиста: Аксенова Тимофея taksenov@gmail.com">
       <header className={classnames('hero hero--primary', styles.heroBanner)}>
         <div className="container">
           <h1 className="hero__title">{siteConfig.title}</h1>
@@ -72,7 +71,7 @@ function Home() {
                 styles.getStarted,
               )}
               to={useBaseUrl('docs/doc1')}>
-              Get Started
+              Поехали!
             </Link>
           </div>
         </div>
